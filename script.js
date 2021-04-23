872be0f6d5d7db9ff27cbee56b5c190c6bf41ac4
Angelica Hernandez <gel.lorraine@gmail.com>
2021-04-23 01:17:12 -0700

    Weather API current and forecast data

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+var cityFormEl = document.querySelector('#city-form');
+var cityInputEl = document.querySelector('#city');
+var forecastContainerEl = document.querySelector('#forecast-container');
+var citySearchTerm = document.querySelector('#city-search-term');
+
+cityInputEl = "Whittier";
+
+currentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl + '&appid=6e5dd978f10ad44dc4fa36386d4e8fdb';
+
+forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInputEl + '&appid=6e5dd978f10ad44dc4fa36386d4e8fdb';
 
 
 
-fetch('https://api.openweathermap.org/data/2.5/find?lat=55.5&lon=37.5&cnt=10&appid=6e5dd978f10ad44dc4fa36386d4e8fdb')
+fetch(currentURL)
     .then(response => response.json())
     .then(data => console.log(data));
 
+fetch(forecastURL)
+    .then(response => response.json())
+    .then(data => console.log(data));
 
